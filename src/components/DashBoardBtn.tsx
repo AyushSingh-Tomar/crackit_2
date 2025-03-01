8b02810409e1758590b928cd2b7ff98992822e92
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2025-03-01 23:27:36 +0530

    Router_Conflict_resolved

--- src/components/DashBoardBtn.tsx
+++ src/components/DashBoardBtn.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useRouter } from 'next/navigation';
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LayoutDashboard } from "lucide-react";
@@ -9,10 +9,16 @@ function DashboardBtn() {
   const { isCandidate, isLoading } = useUserRole();
 
   if (isCandidate || isLoading) return null;
-
+  
+    const router = useRouter();
+  
+    const goToDashboard = () => {
+      router.push('/dashboard');
+    };
   return (
     <Link href={"/dashboard"}>
-      <Button className="gap-2 font-medium" size={"sm"}>
+      <Button className="gap-2 font-medium" size={"sm"}
+      onClick={goToDashboard}>
         <LayoutDashboard className="size-4" />
         Dashboard
       </Button>
